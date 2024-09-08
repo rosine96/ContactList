283e4370c72d1061e7a4f5e425e0a52e740fda22
rosine ngono <ngrosine07@gmail.com>
2024-09-08 03:22:12 +0100

    fonctionnalite addContact on main program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ContactList
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ContactList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           List<Contact> listOfContact = new List<Contact>();
+            int choix=0;
+            Console.WriteLine(@"bienvenue dans ContactList!
+                      quelle operation souhaitez vous effectuer?
+                                1-ajouter un contact
+                                2-rechercher un contact
+                                3-modifier un contact
+                                 4- supprimer un contact
+                                 5-afficher la listes des contacts");
+            choix=int.Parse(Console.ReadLine());
+            switch( choix )
+            {
+                case 1:
+                    Console.WriteLine("entrer le nom");
+                    string name=Console.ReadLine();
+                    Console.WriteLine("entrer le prenom");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("entrerle numero de telephone");
+                    string phone = Console.ReadLine();
+                    try
+                    {
+                        Contact myContact = new Contact(name, firstName, phone);
+                        myContact.Ajouter(listOfContact);
+                    }
+                    catch(Exception ex) {Console.WriteLine(ex.Message); }
+                    
+                    break;
+
+            }
         }
     }
 }
